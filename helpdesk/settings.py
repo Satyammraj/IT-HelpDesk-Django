8c36e32c3fa967888f5cf9632e854b0ca4d0813e
Satyam Raj <samd89544@gmail.com>
2026-07-15 02:22:13 +0530

    Configure production settings for deployment

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^hm+8m&=!k7==399sir-+0%nq2rkxnt$a#(ugfr+kmymzm0364"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-your-current-secret-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
