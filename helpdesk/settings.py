2a9e47265007c2b5d88b2e30b21af6a248caf12e
Satyam Raj <samd89544@gmail.com>
2026-07-15 17:43:12 +0530

    installing brevo smtp instead of google

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -168,16 +168,13 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
